30d4b7c9d28db200454e0e4d0b4d767a69603287
Yiyo Zapata <yiyo.zapata@etoolinnovation.com>
2023-09-04 15:27:00 +0200

    popup

--- micro_blog/app/main/routes.py
+++ micro_blog/app/main/routes.py
@@ -12,6 +12,14 @@ from app.main import bp
 from app.main.forms import SearchForm
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+    return render_template('user_popup.html', user=user, form=form)
+
+
 @bp.route('/search')
 @login_required
 def search():
